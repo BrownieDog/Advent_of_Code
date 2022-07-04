b868f263dadf3ae98d25492132e3fcac5609ac9c
jonse <19jsedgwick@davis.k12.ut.us>
2022-07-03 18:09:15 -0600

    actually close the ifstrem and added post-run file selection

--- 2015/Day1/Puzzle1/src/main.cpp
+++ 2015/Day1/Puzzle1/src/main.cpp
@@ -1,38 +1,63 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 //Takes a file name as a command line argument, then prints the floor santa will end on
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-	if (argc != 2)
+	if (argc > 2)
 	{
 		std::cout << "use as \"[COMMAND] [FILENAME]\"" << std::endl;
-		std::exit(1);
+		exit(1);
 	}
 
-	std::ifstream ifs(argv[1], std::ifstream::in);
-
-
-	char next;
-	int floor = 0;
-	while (ifs.good())
+	std::string filename;
+	if (argc == 2)
 	{
-		next = ifs.get();
-		switch (next)
-		{
-		case '(':
-			floor++;
-			break;
-		case ')':
-			floor--;
-			break;
-		default:
-			//do not change floor if not one of the above symbols
-			break;
-		}
+		filename = argv[1];
+	}
+	else
+	{
+		std::cout << "File with instructions: ";
+		char charbuffer[FILENAME_MAX];
+		std::cin.getline(charbuffer, FILENAME_MAX);
+		filename = charbuffer;
 	}
 
-	std::cout << "Santa would be on floor " << floor << " after following these instructions." << std::endl;
+	std::ifstream ifs;
 
-	return 0;
+	ifs.open(filename, std::ifstream::in);
+
+	if (!ifs.is_open())
+	{
+		std::cout << "File " << filename << " could not be opened" << std::endl;
+		exit(2);
+	}
+	else
+	{
+
+		char next;
+		int floor = 0;
+		while (ifs.good())
+		{
+			next = ifs.get();
+			switch (next)
+			{
+			case '(':
+				floor++;
+				break;
+			case ')':
+				floor--;
+				break;
+			default:
+				//do not change floor if not one of the above symbols
+				break;
+			}
+		}
+
+		ifs.close();
+		std::cout << "Santa would be on floor " << floor << " after following these instructions." << std::endl;
+
+		return 0;
+	}
 }
